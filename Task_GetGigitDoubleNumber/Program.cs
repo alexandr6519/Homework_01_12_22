c611f1ee8aa11ef4ad5c0787482293b9ee95470f
Ikonnikov Aleksandr <alexandr6519@mail.ru>
2022-12-02 13:20:44 +0700

    adding TaskNumberDigitsDouble

---
+++ Task_GetGigitDoubleNumber/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 26. - HARD необязательная Напишите программу, которая принимает на вход целое или дробное число и выдаёт количество цифр в числе.
+// 452 -> 3
+// 82 -> 2
+// 9,012 ->4
+Console.WriteLine("Input any number, please");
+double number = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"The count of digits of {number} is {GetNumberDigitsOfDouble(number)}");
+
+
+int GetNumberDigitsOfDouble(double a)
+{
+    int result = (int)GetNumberDigitsIntNumber((int)(a));
+    if (!HasFractionalPart(number)) return result;
+    else
+    {
+        while (a % 10 != 0)
+        {
+            result++;
+            a *= 10;
+        }
+        return result - 1;
+    }
+}
+
+bool HasFractionalPart(double a)
+{
+    if (a == (int)a) return false;
+    return true;
+}
+
+double GetNumberDigitsIntNumber(int n)
+{
+    if (n < 1) return 0;
+    int nAbs = Math.Abs(n);
+    double result = Math.Log10(nAbs);
+    return (int)result + 1;
+}
